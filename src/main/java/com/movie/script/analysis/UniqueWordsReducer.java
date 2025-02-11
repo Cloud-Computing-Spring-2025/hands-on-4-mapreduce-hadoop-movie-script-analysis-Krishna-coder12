207ec54b630427c411dac1393176cfdffafe6bb5
Krishna-Teja12 <74000968+Krishna-coder12@users.noreply.github.com>
2025-02-11 21:53:24 +0000

    Output

--- src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
+++ src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
@@ -2,7 +2,7 @@ package com.movie.script.analysis;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-
+import java.util.Set;
 import java.io.IOException;
 import java.util.HashSet;
 
@@ -10,6 +10,13 @@ public class UniqueWordsReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        Set<String> uniqueWords = new HashSet<>();
+        for (Text value : values) {
+            String[] words = value.toString().split(",");
+            for (String word : words) {
+                uniqueWords.add(word);
+            }
+        }
+        context.write(key, new Text(uniqueWords.toString()));
     }
 }
